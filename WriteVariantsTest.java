7da417399ab7a2fe3bb31437cd859f3c7dbe332b
mcgml <mcgml@cardiff.ac.uk>
2017-06-16 17:11:01 +0100

    added test

---
+++ WriteVariantsTest.java
@@ -0,0 +1,16 @@
+package nhs.genetics.cardiff;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ml on 16/06/2017.
+ */
+public class WriteVariantsTest {
+    @Test
+    public void toTextFile() throws Exception {
+
+    }
+
+}
