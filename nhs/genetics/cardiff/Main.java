7f7e02159c06f10f19a8ca2c69356996dc44e3ed
Matthew Lyon <ml@Matthews-MacBook-Pro.local>
2017-07-19 13:57:57 +0100

    Removed unneeded imports

--- nhs/genetics/cardiff/Main.java
+++ nhs/genetics/cardiff/Main.java
@@ -17,8 +17,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Program for filtering and writing variants using Apache Spark
