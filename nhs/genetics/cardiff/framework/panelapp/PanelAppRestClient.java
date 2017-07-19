c5c8caacf24dbc2fe2fa1a645f6fb6c20ba1e623
Matthew Lyon <ml@Matthews-MacBook-Pro.local>
2017-07-19 16:02:51 +0100

    Skipping unknown moi enum

--- nhs/genetics/cardiff/framework/panelapp/PanelAppRestClient.java
+++ nhs/genetics/cardiff/framework/panelapp/PanelAppRestClient.java
@@ -1,5 +1,6 @@
 package nhs.genetics.cardiff.framework.panelapp;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -9,7 +10,7 @@ import java.net.URL;
  * REST client for PanelApp
  */
 public class PanelAppRestClient {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
 
     public static PanelAppResponse searchByGene(String gene) throws IOException {
         return objectMapper.readValue(new URL(
