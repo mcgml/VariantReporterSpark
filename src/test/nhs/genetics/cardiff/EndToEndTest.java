4729b89c68e604f609d202cc10f33e9f0ca22ddb
mcgml <mcgml@cardiff.ac.uk>
2018-02-01 10:15:07 +0000

    began refactoring VCF reader to support unit testing

---
+++ src/test/nhs/genetics/cardiff/EndToEndTest.java
@@ -0,0 +1,15 @@
+package nhs.genetics.cardiff;
+
+import org.junit.Test;
+
+/**
+ * Created by ml on 31/01/2018.
+ */
+public class EndToEndTest {
+
+    @Test
+    public void deNovoWorkflow() throws Exception {
+
+    }
+
+}
